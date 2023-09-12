759a7b3bc1504c00323de567ec37d88afb579d38
guozhanyan <jamyanguo@gmail.com>
2023-09-11 23:41:52 -0400

    multiples

---
+++ src/Multiples.java
@@ -0,0 +1,9 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 999;
+        int su = i / 3 + i / 5;
+        System.out.println(su);
+    }
+}
